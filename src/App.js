0a3e315fc698e27139b8b0400887a02911ec29ff
Moni  Gupta <72276446+moni310@users.noreply.github.com>
2022-05-13 17:33:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import GoogleLogin from 'react-google-login';
+import FacebookLogin from 'react-facebook-login';
 import './App.css';
+import axios from "axios"
 
 function App() {
-  return (
+  
+  const responseSuccessGoogle = async (res) => {
+    console.log(res.Lu)
+    let info = await axios.post(
+      `http://localhost:4000/mm/singup`,
+      {
+        username:res.Lu.tf,
+        email: res.Lu.Bv
+      },
+    );
+    console.log(info, "data")
+    
+  }
+  const responseErrorGoogle =(res)=>{
+  }
+
+  const responseFacebook = async (res)=>{
+    console.log(res)
+
+    console.log(res.name)
+    let info = await axios.post(
+      `http://localhost:4000/mm/singup`,
+      {
+        name:res.name,
+        userID:res.userID
+      },
+    );
+    console.log(info, "data")
+  }
+
+return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <h1>
+          Login with Google
+        </h1>
+
+
+        <GoogleLogin
+    clientId="292402598329-hg10fnjl6panpmbnatfrtk5pkumq53dd.apps.googleusercontent.com"
+    buttonText="  Login with Google   "
+    onSuccess={responseSuccessGoogle}
+    onFailure={responseErrorGoogle}
+    cookiePolicy={'single_host_origin'}
+  />
+    </div>
+    <h1>
+      Login with facebook</h1>
+      <FacebookLogin
+      appId="2993180237569264"
+      autoLoad={true}
+      callback={responseFacebook} />
     </div>
   );
 }
